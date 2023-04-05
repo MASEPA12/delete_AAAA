758d4a99d2e1aa9aaf9cc232c8cf01f0dce711c9
MASEPA12 <114388814+MASEPA12@users.noreply.github.com>
2023-04-05 21:33:49 +0200

    initialComit

---
+++ Assets/playerController.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float walkingForce = 0.5f;
+   
+    public float jumpingForce = 0.2f;
+    private float gravityModifier = 1.5f;
+
+    //private bool isJumping;
+    public Animator animator;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        Physics.gravity *= gravityModifier;
+        rb = GetComponent<Rigidbody>();
+        //isJumping = false;
+    }
+
+    private void Update()
+    {
+        bool crouchPressed = Input.GetKey("w");
+
+        if (crouchPressed)
+        {
+            animator.SetBool("isSteady", false); //ja no esta`dret
+
+        }
+        if (!crouchPressed)
+        {
+            animator.SetBool("isSteady", true); //si no pitj, està dret
+             
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            rb.AddForce(Vector3.forward * walkingForce, ForceMode.Force);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(Vector3.up * jumpingForce, ForceMode.Impulse);
+            animator.SetBool("isJumping", true);
+
+        }
+
+        if (!Input.GetKey(KeyCode.Space))
+        {
+            animator.SetBool("isJumping", false);
+        }
+
+    }
+}
+
+
